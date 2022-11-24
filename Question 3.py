fd104c3330298bae2749cbd2acec7358b66a4de3
fldyyxy <118820416+fldyyxy@users.noreply.github.com>
2022-11-24 17:09:02 +0700

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,35 @@
+#I have no experience with automation testing but i try my best to create automation scripts. I created automation script use Selenium and Python. 
+#Here is the detail:
+
+#Test case: User want to register on Facebook. 
+#Test data:
+ # •	First name: Faldinando 
+ # •	Surname : Malelak
+ # •	Email address: faldomalelak251199@gmail.com
+ # •	Password: Tester0000
+ # •	Date of birth: May 7th, 1999
+ # •	Gender: Male
+ 
+#Here is the script I create with selenium and pyhton
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://en-gb.facebook.com/reg/')
+driver.implicitly_wait(10)
+driver.find_element(By.NAME,"firstname").send_keys("Faldinando")
+driver.find_element(By.NAME,"lastname").send_keys("Malelak")
+driver.find_element(By.NAME,"reg_email__").send_keys("faldinando251199@gmail.com")
+driver.find_element(By.NAME,"reg_email_confirmation__").send_keys("faldinando251199@gmail.com")
+driver.find_element(By.ID,"password_step_input").send_keys("Tester0000")
+sel = Select(driver.find_element(By.XPATH,"//select[@title='Day']"))
+sel.select_by_visible_text("7")
+sel = Select(driver.find_element(By.XPATH,"//select[@title='Month']"))
+sel.select_by_visible_text("May")
+sel = Select(driver.find_element(By.XPATH,"//select[@title='Year']"))
+sel.select_by_visible_text("1999")
+driver.find_element(By.XPATH,"//label[text()='Male']").click()
+driver.find_element(By.XPATH,"//button[text()='Sign Up']").click()
